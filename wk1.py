9c7bcbf36e6c11e26d555abbc0608e3fc2e24d46
Dan Polityka <73758508+benochi@users.noreply.github.com>
2025-02-25 09:42:27 -0600

    notes

--- wk1.py
+++ wk1.py
@@ -303,3 +303,14 @@ except FileNotFoundError:
 finally:
     file.close()
     print("File closed.")
+
+# raise keyword. 
+def check_age(age):
+    if age < 18:
+        raise ValueError("Age must be 18 or older.")
+    return True
+
+try:
+    check_age(16)
+except ValueError as e:
+    print(e)
